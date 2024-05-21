80cadddc7799343a3df46d57f0f62da85e0a6d9b
ijlalWindhi <ijlalwindhi15@gmail.com>
2024-05-22 00:05:28 +0700

    feat(home):map component with handle hover and click

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
-import Image from "next/image";
-
+import Map from "@/components/shared/map";
 export default function Home() {
   return (
     <main className="flex flex-col justify-center relative items-center min-h-screen w-full bg-black text-white">
-      <h1 className="font-semibold text-2xl absolute top-14">
+      <h1 className="font-semibold text-5xl text-[#ff0000] absolute top-14">
         Pusaka Cakrawala
       </h1>
-      <Image
-        src="/images/map.svg"
-        alt="Pusaka Cakrawala"
-        layout="fill"
-        objectFit="contain"
-        priority
-      />
+      <div className="w-[80%]">
+        <Map />
+      </div>
     </main>
   );
 }
